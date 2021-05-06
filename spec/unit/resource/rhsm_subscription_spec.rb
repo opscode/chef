36730409af8c557be89913056762fd93b05c031b
Jason Barnett <jason.w.barnett@gmail.com>
2021-05-06 14:00:09 -0600

    Flush package cache after attaching to rhsm subscription
    Signed-off-by: Jason Barnett <jason.w.barnett@gmail.com>

--- spec/unit/resource/rhsm_subscription_spec.rb
+++ spec/unit/resource/rhsm_subscription_spec.rb
@@ -18,15 +18,25 @@
 require "spec_helper"
 
 describe Chef::Resource::RhsmSubscription do
-  let(:resource) { Chef::Resource::RhsmSubscription.new("fakey_fakerton") }
-  let(:provider) { resource.provider_for_action(:attach) }
+  let(:event_dispatch) { Chef::EventDispatch::Dispatcher.new }
+  let(:node) { Chef::Node.new }
+  let(:run_context) { Chef::RunContext.new(node, {}, event_dispatch) }
+
+  let(:pool_id) { "8a8dd78c766232550226b46e59404aba" }
+  let(:resource) { Chef::Resource::RhsmSubscription.new(pool_id, run_context) }
+  let(:provider) { resource.provider_for_action(resource.action) }
+
+  before do
+    # Ignore all shell_out! calls
+    allow(provider).to receive(:shell_out!)
+  end
 
   it "has a resource name of :rhsm_subscription" do
     expect(resource.resource_name).to eql(:rhsm_subscription)
   end
 
   it "the pool_id property is the name_property" do
-    expect(resource.pool_id).to eql("fakey_fakerton")
+    expect(resource.pool_id).to eql(pool_id)
   end
 
   it "sets the default action as :attach" do
@@ -36,6 +46,35 @@ describe Chef::Resource::RhsmSubscription do
   it "supports :attach, :remove actions" do
     expect { resource.action :attach }.not_to raise_error
     expect { resource.action :remove }.not_to raise_error
+  end
+
+  describe "#action_attach" do
+    context "when already attached to pool" do
+      before do
+        allow(provider).to receive(:subscription_attached?).with(resource.pool_id).and_return(true)
+      end
+
+      it "does not attach to pool" do
+        expect(provider).not_to receive(:shell_out!).with("subscription-manager attach --pool=#{resource.pool_id}")
+        resource.run_action(:attach)
+      end
+    end
+
+    context "when not attached to pool" do
+      before do
+        allow(provider).to receive(:subscription_attached?).with(resource.pool_id).and_return(false)
+      end
+
+      it "attaches to pool" do
+        expect(provider).to receive(:shell_out!).with("subscription-manager attach --pool=#{resource.pool_id}")
+        resource.run_action(:attach)
+      end
+
+      it "flushes yum cache" do
+        expect(provider).to receive(:flush_cache)
+        resource.run_action(:attach)
+      end
+    end
   end
 
   describe "#subscription_attached?" do
